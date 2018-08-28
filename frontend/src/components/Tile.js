6b9bb6f683a6525ab8d439d871f8e106e870194c
Dinko Harbinja <dinko.harbinja@flicksoftware.com>
2018-08-28 17:52:21 -0400

    Getting the tiles to show based on back-end data, but no event handling yet.

--- frontend/src/components/Tile.js
+++ frontend/src/components/Tile.js
@@ -3,9 +3,16 @@ import { Button } from 'react-bootstrap';
 
 class Tile extends React.Component {
     render() {
+        let content = ''
+        if (this.props.tile.status === 'Opened') {
+        content = this.props.tile.is_mine ? 'B' : this.props.tile.neighbouring_mines;
+        } else if (this.props.tile.status === 'Flagged') {
+            content = 'F';
+        }
+
         return (
             <Button className="minesweeper-tile">
-                {this.props.tile.id}
+                {content}
             </Button>
         )
     }
