1af5c325a63d4b35cadab425fe2f249baf9bb8fa
Dinko Harbinja <dinko.harbinja@flicksoftware.com>
2018-08-30 00:46:50 -0400

    Don't need error twice

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
       difficulties: [],
       selectedDifficultyId: null,
       isLoading: false,
-      error: null,
       minesweeperService: new MinesweeperService()
     }
   }
